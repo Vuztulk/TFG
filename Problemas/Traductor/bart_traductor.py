dd9b9930aa54a1737f2da1774ab380a52fd4686b
Luis <luisbalo2001@gmail.com>
2024-01-30 14:01:17 +0100

    bart

--- Problemas/Traductor/bart_traductor.py
+++ Problemas/Traductor/bart_traductor.py
@@ -10,12 +10,12 @@ with open('./Problemas/Predictor de Texto/input.txt', 'r') as file:
     input_text = file.read().replace('\n', '')
 
 # Codificar entrada
-input_ids = tokenizer(input_text, return_tensors='pt', src_lang="es_XX").input_ids
+input_ids = tokenizer(">>en_XX<<" + input_text, return_tensors='pt').input_ids
 
 # Realizar la inferencia del modelo
 with torch.no_grad():
     outputs = model.generate(input_ids, max_length=200, num_return_sequences=1, decoder_start_token_id=model.config.pad_token_id)
 
 # Decodificar la salida
-output_text = tokenizer.decode(outputs[0], skip_special_tokens=True, tgt_lang="en_XX")
+output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(f'Texto de salida: {output_text}')

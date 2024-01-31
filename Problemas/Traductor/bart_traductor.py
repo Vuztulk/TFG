ff032da1faff3ccdd9de5247a0a0bcd85e0d088c
Luis <luisbalo2001@gmail.com>
2024-01-31 14:30:48 +0100

    bart

--- Problemas/Traductor/bart_traductor.py
+++ Problemas/Traductor/bart_traductor.py
@@ -5,29 +5,30 @@ import psutil
 import os
 
 # Cargar el tokenizador y el modelo
-tokenizer = MBart50TokenizerFast.from_pretrained('facebook/mbart-large-50-many-to-many-mmt')
-model = MBartForConditionalGeneration.from_pretrained('facebook/mbart-large-50-many-to-many-mmt')
+tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
 
 # Leer el texto de entrada desde un archivo .txt
 with open('./Problemas/Predictor de Texto/input.txt', 'r') as file:
     input_text = file.read().replace('\n', '')
 
 # Codificar entrada
-input_ids = tokenizer(input_text, return_tensors='pt').input_ids
+tokenizer.src_lang = "es_XX"
+encoded_es = tokenizer(input_text, return_tensors="pt")
 
 # Realizar la inferencia del modelo con el perfilador
 with torch.no_grad():
     with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
         with record_function("model_inference"):
-            outputs = model.generate(input_ids, forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"])
+            generated_tokens = model.generate(**encoded_es, forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"])
 
 # Imprimir las métricas del perfilador
 print("Métricas del perfilador:")
 print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
 
 # Decodificar la salida
-output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(f'Texto de salida: {output_text}')
+output_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+print(f'Output text: {output_text}')
 
 # Métricas adicionales
 pid = os.getpid()

b9cdab25757d5585c86a586c74c99228777e92c3
Luis <luisbalo2001@gmail.com>
2024-02-06 17:25:36 +0100

    bert2

--- Problemas/Clasificacion estrellas/bert_2_estrellas.py
+++ Problemas/Clasificacion estrellas/bert_2_estrellas.py
@@ -5,8 +5,8 @@ import psutil
 import os
 
 # Cargamos el modelo y el tokenizador preentrenados
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
-model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
+tokenizer = AutoTokenizer.from_pretrained('dccuchile/bert-base-spanish-wwm-cased')
+model = AutoModelForSequenceClassification.from_pretrained('dccuchile/bert-base-spanish-wwm-cased')
 
 with open('/home/tfg1/TFG/Problemas/Clasificacion estrellas/input.txt', 'r') as file:
     input_text = file.read().strip()
@@ -27,15 +27,15 @@ print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
 
 # Imprimimos la clase predicha
 sentiment_classes = ['1 estrella', '2 estrellas', '3 estrellas', '4 estrellas', '5 estrellas']
-print(f'Input text: {input_text}')
-print(f'Predicted sentiment: {sentiment_classes[predicted_class]}')
+print(f'Texto de entrada: {input_text}')
+print(f'Sentimiento predicho: {sentiment_classes[predicted_class]}')
 
 # Métricas adicionales
 pid = os.getpid()
 py = psutil.Process(pid)
 
 memory_use = py.memory_info()[0]/2.**30  # memory use in GB
-print(f'Memory use: {memory_use} GB')
+print(f'Uso de memoria: {memory_use} GB')
 
 cpu_use = psutil.cpu_percent(interval=None)
-print(f'CPU use: {cpu_use} %')
+print(f'Uso de CPU: {cpu_use} %')

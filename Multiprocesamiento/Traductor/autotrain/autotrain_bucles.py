949b1731630d71e4cf88134472e5d6f54d4eb1e8
Luis <luisbalo2001@gmail.com>
2024-02-13 14:15:02 +0100

    a

--- Multiprocesamiento/Traductor/autotrain/autotrain_bucles.py
+++ Multiprocesamiento/Traductor/autotrain/autotrain_bucles.py
@@ -18,7 +18,7 @@ input_ids = tokenizer.encode(input_text, return_tensors='pt')
 # Abrir el archivo de resultados
 with open('resultados.txt', 'w') as f:
     # Ejecutar el código 20 veces
-    for i in range(20):
+    for i in range(2):
         start_time = time.time()
 
         # Realizar la inferencia del modelo con el perfilador
@@ -28,9 +28,9 @@ with open('resultados.txt', 'w') as f:
                     outputs = model.generate(input_ids, max_length=200, num_return_sequences=1)
 
         # Guardar las métricas del perfilador en el archivo
-        #f.write("Métricas del perfilador:\n")
-        #f.write(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
-        #f.write('\n')
+        cpu_time = sum([item.cpu_time_total for item in prof.key_averages()])
+        cpu_time_seconds = cpu_time / 1_000_000
+        f.write(f'{cpu_time_seconds}\n')
 
         # Decodificar la salida
         output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
@@ -38,15 +38,15 @@ with open('resultados.txt', 'w') as f:
         #f.write(f'Texto de salida: {output_text}\n')
 
         # Métricas adicionales
-        pid = os.getpid()
-        py = psutil.Process(pid)
+        #pid = os.getpid()
+        #py = psutil.Process(pid)
 
-        memory_use = py.memory_info()[0]/2.**30  # memory use in GB
+        #memory_use = py.memory_info()[0]/2.**30  # memory use in GB
         #f.write(f'Memory use: {memory_use} GB\n')
 
-        cpu_use = psutil.cpu_percent(interval=None)
+        #cpu_use = psutil.cpu_percent(interval=None)
         #f.write(f'CPU use: {cpu_use} %\n')
 
         end_time = time.time()
         duration = end_time - start_time
-        f.write(f'La ejecución del código tardó {duration:.4f} segundos.\n\n')
+        f.write(f'{duration:.4f}\n\n')

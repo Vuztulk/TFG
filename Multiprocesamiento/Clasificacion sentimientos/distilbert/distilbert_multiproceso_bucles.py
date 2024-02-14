e69c7504cde4b50b783ff0e5c93126039fde3325
Luis <luisbalo2001@gmail.com>
2024-02-14 14:26:16 +0100

    a

--- Multiprocesamiento/Clasificacion sentimientos/distilbert/distilbert_multiproceso_bucles.py
+++ Multiprocesamiento/Clasificacion sentimientos/distilbert/distilbert_multiproceso_bucles.py
@@ -4,10 +4,19 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import psutil
 import os
 import time
+import concurrent.futures
 
 # Cargamos el modelo y el tokenizador preentrenados
 tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
 model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
+
+# Función para realizar la inferencia del modelo
+def model_inference(encoded_input):
+    with torch.no_grad():
+        outputs = model(**encoded_input)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits).item()
+    return predicted_class
 
 # Abrimos el archivo de resultados
 with open('resultados.txt', 'w') as f:
@@ -21,19 +30,17 @@ with open('resultados.txt', 'w') as f:
 
         encoded_input = tokenizer(input_text, return_tensors='pt')
 
-        # Inicializamos el perfilador de PyTorch
-        with torch.no_grad():
+        # Realizamos la inferencia del modelo con el perfilador en paralelo
+        with concurrent.futures.ProcessPoolExecutor() as executor:
             with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
                 with record_function("model_inference"):
-                    outputs = model(**encoded_input)
-                    logits = outputs.logits
-                    predicted_class = torch.argmax(logits).item()
+                    future = executor.submit(model_inference, encoded_input)
+                    predicted_class = future.result()
 
         # Guardamos las métricas del perfilador en el archivo
         cpu_time = sum([item.cpu_time_total for item in prof.key_averages()])
         cpu_time_seconds = cpu_time / 1_000_000
-        cpu_time_str = str(cpu_time_seconds).replace('.', ',') 
-        f.write(f'{cpu_time_str:.4f}\n')
+        f.write(f'{cpu_time_seconds}\n')
 
         # Imprimimos la clase predicha
         sentiment_classes = ['negative', 'positive']
@@ -52,5 +59,4 @@ with open('resultados.txt', 'w') as f:
 
         end_time = time.time()
         duration = end_time - start_time
-        duration_str = f'{duration:.4f}'.replace('.', ',')
-        f.write(f'{duration_str:.4f}\n')
+        f.write(f'{duration:.4f}\n')

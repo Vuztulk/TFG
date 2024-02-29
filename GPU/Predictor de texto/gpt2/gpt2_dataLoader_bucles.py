ab94f759cdf22341830667d21522427553c22828
Vuztulk <luisbalo2001@gmail.com>
2024-02-29 14:28:28 +0100

    a

--- GPU/Predictor de texto/gpt2/gpt2_dataLoader_bucles.py
+++ GPU/Predictor de texto/gpt2/gpt2_dataLoader_bucles.py
@@ -15,10 +15,16 @@ class TextDataset(Dataset):
     def __getitem__(self, idx):
         return self.text[idx]
 
+# Verificamos si hay una GPU disponible y, en caso afirmativo, la usamos. Si no, usamos la CPU.
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 # Cargar el tokenizador y el modelo
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 model.config.pad_token_id = model.config.eos_token_id
+
+# Movemos el modelo a la GPU
+model = model.to(device)
 
 # Crear un DataLoader con paralelización a nivel de datos
 dataset = TextDataset('/home/tfg1/TFG/Problemas/Predictor de Texto/input.txt')
@@ -32,20 +38,20 @@ with open('resultados.txt', 'w') as f:
 
         # Realizar la inferencia del modelo con el perfilador
         with torch.no_grad():
-            with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+            with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], record_shapes=True) as prof:
                 with record_function("model_inference"):
                     for i, input_text in enumerate(dataloader):
-                        input_ids = tokenizer.encode(input_text[0], return_tensors='pt')
-                        attention_mask = torch.ones(input_ids.shape)
+                        input_ids = tokenizer.encode(input_text[0], return_tensors='pt').to(device)
+                        attention_mask = torch.ones(input_ids.shape, device=device)
                         outputs = model.generate(input_ids, max_length=100, temperature=0.7, num_return_sequences=1, do_sample=True, attention_mask=attention_mask)
 
         # Guardamos las métricas del perfilador en el archivo
         model_inference_event = [item for item in prof.key_averages() if item.key == "model_inference"]
         if model_inference_event:
-            cpu_time = model_inference_event[0].cpu_time_total
-            cpu_time_seconds = cpu_time / 1_000_000
-            cpu_time_str = f'{cpu_time_seconds:.4f}'.replace('.', ',')
-            f.write(f'{cpu_time_str}\n')
+            gpu_time = model_inference_event[0].cuda_time_total
+            gpu_time_seconds = gpu_time / 1_000_000
+            gpu_time_str = f'{gpu_time_seconds:.4f}'.replace('.', ',')
+            f.write(f'{gpu_time_str}\n')
 
         output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
         

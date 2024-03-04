23e893b6cb37377a1e25a7e0df5a744c77e0b2fa
Vuztulk <luisbalo2001@gmail.com>
2024-03-04 22:29:49 +0100

    a

---
+++ Aplicacion/Placa.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from Modelos.Clasificacion_Sentimientos import clasificacion_sentimiento
+from Modelos.Traduccion import traduccion_texto
+
+app = Flask(__name__)
+
+@app.route('/procesar_texto', methods=['POST'])
+
+def recibir_texto():
+    if request.method == 'POST':
+        texto = request.form['texto1']
+        if 'submit_clasificacion' in request.form:
+            resultado = clasificacion_sentimiento(texto)
+        elif 'submit_traduccion' in request.form:
+            resultado = traduccion_texto(texto)
+        else:
+            resultado = 'Acción desconocida'
+        return resultado
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

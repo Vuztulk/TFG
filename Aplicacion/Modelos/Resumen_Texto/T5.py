88623537fce098186c20c9de2a2dd611338ba1b4
Vuztulk <luisbalo2001@gmail.com>
2024-03-13 12:43:34 +0100

    a

--- Aplicacion/Modelos/Resumen_Texto/T5.py
+++ Aplicacion/Modelos/Resumen_Texto/T5.py
@@ -34,4 +34,32 @@ def res_t5_cpu(input_text):
     return summary_text, cpu_time_str, formatted_duration
 
 def res_t5_gpu(input_text):
-    return 0
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    start_time = time.time()
+    
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    model = model.to(device)
+    
+    inputs = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=1024, truncation=True).to(device)
+
+    with torch.no_grad():
+        with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], record_shapes=True) as prof:
+            with record_function("model_inference"):
+                summary_ids = model.generate(inputs, max_length=100, min_length=30, num_beams=4, early_stopping=True)
+
+    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    
+    model_inference_event = [item for item in prof.key_averages() if item.key == "model_inference"]
+    if model_inference_event:
+        gpu_time = model_inference_event[0].cuda_time_total
+        gpu_time_seconds = gpu_time / 1_000_000
+        gpu_time_str = f'{gpu_time_seconds:.4f}'.replace('.', ',')
+            
+    end_time = time.time()
+    duration = end_time - start_time
+    formatted_duration = f'{duration:.4f}'.replace('.', ',')
+
+    return summary_text, gpu_time_str, formatted_duration
--- Aplicacion/Modelos/Resumen_Texto/bart.py
+++ Aplicacion/Modelos/Resumen_Texto/bart.py
@@ -30,4 +30,30 @@ def res_bart_cpu(input_text):
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True), cpu_time_str, formatted_duration
 
 def res_bart_gpu(input_text):
-    return 0
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+    start_time = time.time()
+    
+    tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+    model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+    model = model.to(device)
+    
+    inputs = tokenizer([input_text], max_length=1024, return_tensors="pt", truncation=True).to(device)
+    
+    with torch.no_grad():
+        with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], record_shapes=True) as prof:
+            with record_function("model_inference"):
+                summary_ids = model.generate(inputs.input_ids, num_beams=4, min_length=30, max_length=100, early_stopping=True)
+                
+    model_inference_event = [item for item in prof.key_averages() if item.key == "model_inference"]
+    if model_inference_event:
+        gpu_time = model_inference_event[0].cuda_time_total
+        gpu_time_seconds = gpu_time / 1_000_000
+        gpu_time_str = f'{gpu_time_seconds:.4f}'.replace('.', ',')
+            
+    end_time = time.time()
+    duration = end_time - start_time
+    formatted_duration = f'{duration:.4f}'.replace('.', ',')
+
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True), gpu_time_str, formatted_duration

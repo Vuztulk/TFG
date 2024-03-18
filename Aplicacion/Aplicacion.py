5dd50f068bcb15067cbd35a1c95e1d05138ef30d
Vuztulk <luisbalo2001@gmail.com>
2024-03-18 16:59:14 +0100

    a

--- Aplicacion/Aplicacion.py
+++ Aplicacion/Aplicacion.py
@@ -3,20 +3,19 @@ import requests
 
 app = Flask(__name__)
 
-def procesar_solicitud(accion, placa, texto, modelo, longitud, procesador = 'cpu'):
-    
-    if placa == 'Local':
-        url = 'http://127.0.0.1:8888'
-    elif placa == 'Raspberry':
-        url = 'http://sardina.dacya.ucm.es:16992'
-    elif placa == 'Orin-CPU':
-        url = 'http://sardina.dacya.ucm.es:16992'
-    elif placa == 'Orin-GPU':
-        url = 'http://sardina.dacya.ucm.es:16992'
-        procesador = 'gpu'
-    else:
+URLS = {
+    'Local': 'http://127.0.0.1:8888',
+    'Raspberry': 'http://sardina.dacya.ucm.es:16992',
+    'Orin-CPU': 'http://sardina.dacya.ucm.es:16992',
+    'Orin-GPU': 'http://sardina.dacya.ucm.es:16992'
+}
+
+def procesar_solicitud(accion, placa, texto, modelo, longitud, procesador='cpu'):
+    url = URLS.get(placa)
+    if not url:
         return "Placa no reconocida"
-    
+    if placa == 'Orin-GPU':
+        procesador = 'gpu'
     response = requests.post(url, data={'accion': accion, 'texto': texto, 'modelo': modelo, 'longitud': longitud, 'procesador': procesador})
     return response.json()
 
@@ -24,83 +23,29 @@ def procesar_solicitud(accion, placa, texto, modelo, longitud, procesador = 'cpu
 def index():
     return render_template('index.html')
 
-@app.route('/traductor', methods=['GET', 'POST'])
-def traductor():
+@app.route('/<ruta>', methods=['GET', 'POST'])
+def ruta(ruta):
     if request.method == 'POST':
+        
         texto = request.form.get('texto')
         placa = request.form.get('placa')
         modelo = request.form.get('modelo')
-        response_data = procesar_solicitud('traduccion', placa, texto, modelo, 0)
+        longitud = request.form.get('longitud', 0)
+        
+        accion = 'traduccion' if ruta == 'traductor' else 'clasificacion' if ruta == 'clasificacion_sentimientos' else 'predictor' if ruta == 'predictor_texto' else 'resumen'
+        response_data = procesar_solicitud(accion, placa, texto, modelo, longitud)
+        
         resultado = response_data.get('resultado', '')
         t_cpu = response_data.get('t_cpu', '')
         t_total = response_data.get('t_total', '')
         memoria = response_data.get('memoria', '')
-
+        
         if memoria < 0.000:
             memoria = "Memoria utilizada demasiado baja"
-
-        return render_template('traductor.html', resultado=resultado, placa=placa, t_cpu=t_cpu, t_total=t_total, modelo=modelo, memoria=memoria)
+            
+        return render_template(f'{ruta}.html', resultado=resultado, placa=placa, t_cpu=t_cpu, t_total=t_total, modelo=modelo, longitud=longitud, memoria=memoria)
     
-    return render_template('traductor.html')
-
-@app.route('/clasificacion_sentimientos', methods=['GET', 'POST'])
-def clasificacion_sentimientos():
-    if request.method == 'POST':
-        texto = request.form.get('texto')
-        placa = request.form.get('placa')
-        modelo = request.form.get('modelo')
-        response_data = procesar_solicitud('clasificacion', placa, texto, modelo, 0)
-        resultado = response_data.get('resultado', '')
-        t_cpu = response_data.get('t_cpu', '')
-        t_total = response_data.get('t_total', '')
-        memoria = response_data.get('memoria', '')
-
-        if memoria < 0.000:
-            memoria = "Memoria utilizada demasiado baja"
-
-        return render_template('clasificacion_sentimientos.html', resultado=resultado, placa=placa, t_cpu=t_cpu, t_total=t_total, modelo=modelo, memoria=memoria)
-    
-    return render_template('clasificacion_sentimientos.html')
-
-@app.route('/predictor_texto', methods=['GET', 'POST'])
-def predictor_texto():
-    if request.method == 'POST':
-        texto = request.form.get('texto')
-        placa = request.form.get('placa')
-        modelo = request.form.get('modelo')
-        longitud = request.form.get('longitud')
-        response_data = procesar_solicitud('predictor', placa, texto, modelo, longitud)
-        resultado = response_data.get('resultado', '')
-        t_cpu = response_data.get('t_cpu', '')
-        t_total = response_data.get('t_total', '')
-        memoria = response_data.get('memoria', '')
-
-        if memoria < 0.000:
-            memoria = "Memoria utilizada demasiado baja"
-
-        return render_template('predictor_texto.html', resultado=resultado, placa=placa, t_cpu=t_cpu, t_total=t_total, modelo=modelo, longitud=longitud, memoria=memoria)
-    
-    return render_template('predictor_texto.html')
-
-@app.route('/resumen_texto', methods=['GET', 'POST'])
-def resumen_texto():
-    if request.method == 'POST':
-        texto = request.form.get('texto')
-        placa = request.form.get('placa')
-        modelo = request.form.get('modelo')
-        longitud = request.form.get('longitud')
-        response_data = procesar_solicitud('resumen', placa, texto, modelo, longitud)
-        resultado = response_data.get('resultado', '')
-        t_cpu = response_data.get('t_cpu', '')
-        t_total = response_data.get('t_total', '')
-        memoria = response_data.get('memoria', '')
-
-        if memoria < 0.000:
-            memoria = "Memoria utilizada demasiado baja"
-
-        return render_template('resumen_texto.html', resultado=resultado, placa=placa, t_cpu=t_cpu, t_total=t_total, modelo=modelo, longitud=longitud, memoria=memoria)
-    
-    return render_template('resumen_texto.html')
+    return render_template(f'{ruta}.html')
 
 if __name__ == '__main__':
     app.run()

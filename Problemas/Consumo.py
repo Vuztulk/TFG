95df6fd5003d7a6a591c1f0819c6acdde7872f5f
Vuztulk <luisbalo2001@gmail.com>
2024-03-21 17:43:25 +0100

    a

--- Problemas/Consumo.py
+++ Problemas/Consumo.py
@@ -9,14 +9,22 @@ power_values_sys = [float(re.search('VIN_SYS_5V0 (\d+)mW', line).group(1)) for l
 
 power_values_gpu = [float(re.search('VDD_GPU_SOC (\d+)mW', line).group(1)) for line in lines]
 
-average_power = sum(power_values_cpu) / len(power_values_cpu)
+average_power_cpu = sum(power_values_cpu) / len(power_values_cpu)
+max_cpu = max(power_values_cpu)
 
-average_power = sum(power_values_sys) / len(power_values_sys)
+average_power_sys = sum(power_values_sys) / len(power_values_sys)
+max_sys = max(power_values_sys)
 
-average_power = sum(power_values_gpu) / len(power_values_gpu)
+average_power_gpu = sum(power_values_gpu) / len(power_values_gpu)
+max_gpu = max(power_values_gpu)
 
-print(f'El consumo medio de energía de CPU: {power_values_cpu} mW')
+print(f'El consumo medio de energía de CPU: {average_power_cpu} mW')
+print(f'El consumo maximo de energía de CPU: {max_cpu} mW')
 
-print(f'El consumo medio de energía de SYS: {power_values_sys} mW')
 
-print(f'El consumo medio de energía de GPU: {power_values_gpu} mW')
+print(f'El consumo medio de energía de SYS: {average_power_sys} mW')
+print(f'El consumo maximo de energía de SYS: {max_sys} mW')
+
+
+print(f'El consumo medio de energía de GPU: {average_power_gpu} mW')
+print(f'El consumo maximo de energía de GPU: {max_gpu} mW')

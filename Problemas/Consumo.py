9f27e007178838754902e49076d6d7ca96c17930
Vuztulk <luisbalo2001@gmail.com>
2024-04-03 23:43:51 +0200

    a

--- Problemas/Consumo.py
+++ Problemas/Consumo.py
@@ -11,13 +11,13 @@ with open('Problemas/tegrastats', 'r') as file:
         match_gpu = re.search(r'VDD_GPU_SOC (\d+)mW', line)
 
         if match_cpu:
-            power_values_cpu.append(int(match_cpu.group(1)))
+            power_values_cpu.append(int(match_cpu.group(1)) / 1000)  # Convertir de mW a W
         
         if match_sys:
-            power_values_sys.append(int(match_sys.group(1)))
+            power_values_sys.append(int(match_sys.group(1)) / 1000)  # Convertir de mW a W
         
         if match_gpu:
-            power_values_gpu.append(int(match_gpu.group(1)))
+            power_values_gpu.append(int(match_gpu.group(1)) / 1000)  # Convertir de mW a W
 
 # Calcular el consumo medio y máximo de energía para CPU, SYS y GPU
 average_power_cpu = sum(power_values_cpu) / len(power_values_cpu) if power_values_cpu else 0
@@ -29,12 +29,7 @@ max_sys = max(power_values_sys) if power_values_sys else 0
 average_power_gpu = sum(power_values_gpu) / len(power_values_gpu) if power_values_gpu else 0
 max_gpu = max(power_values_gpu) if power_values_gpu else 0
 
-# Imprimir los resultados
-print(f'El consumo medio de energía de CPU: {average_power_cpu} mW')
-print(f'El consumo máximo de energía de CPU: {max_cpu} mW')
-
-print(f'El consumo medio de energía de SYS: {average_power_sys} mW')
-print(f'El consumo máximo de energía de SYS: {max_sys} mW')
-
-print(f'El consumo medio de energía de GPU: {average_power_gpu} mW')
-print(f'El consumo máximo de energía de GPU: {max_gpu} mW')
+# Imprimir los valores en el formato requerido
+print(f'{average_power_cpu:.2f} - {max_cpu:.2f} ')
+print(f'{average_power_sys:.2f} - {max_sys:.2f} ')
+print(f'{average_power_gpu:.2f} - {max_gpu:.2f} ')

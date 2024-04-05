9fb099f2a67a60562984ef5df0c4687a4e908ded
Vuztulk <luisbalo2001@gmail.com>
2024-04-05 20:03:35 +0200

    a

--- Comsumo/Traductor/cartenisus/cartesinus_bucles.py
+++ Comsumo/Traductor/cartenisus/cartesinus_bucles.py
@@ -19,7 +19,7 @@ with open('resultados.txt', 'w') as f:
 
         with torch.no_grad(), profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
             with record_function("model_inference"):
-                process_tegra = subprocess.Popen(['sudo', '/usr/bin/tegrastats' '-logfile' 'tegrastats.txt'])
+                process_tegra = subprocess.Popen(['sudo', '/usr/bin/tegrastats', '-logfile', 'tegrastats.txt'])
                 #process_tegra = subprocess.Popen('/usr/bin/tegrastats', stdout=f, stderr=subprocess.PIPE)
                     
                 generated_tokens = model.generate(input_ids=input_ids, forced_bos_token_id=tokenizer.get_lang_id("es"))

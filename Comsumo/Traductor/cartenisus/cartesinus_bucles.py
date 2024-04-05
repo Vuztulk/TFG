581c9aa05249d7123d73466e432609553acad7e0
Vuztulk <luisbalo2001@gmail.com>
2024-04-05 19:01:44 +0200

    a

--- Comsumo/Traductor/cartenisus/cartesinus_bucles.py
+++ Comsumo/Traductor/cartenisus/cartesinus_bucles.py
@@ -21,22 +21,19 @@ with open('resultados.txt', 'w') as f:
         # Codificar entrada
         input_ids = tokenizer.encode(input_text, return_tensors='pt')
 
-        # Ejecutar tegrastats antes de la inferencia
         #process_tegra = subprocess.Popen(['sudo', '/usr/bin/tegrastats'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process_tegra = subprocess.Popen('/usr/bin/tegrastats', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        try:
-            # Realizar la inferencia del modelo
-            with torch.no_grad(), profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
-                with record_function("model_inference"):
-                    generated_tokens = model.generate(input_ids=input_ids, forced_bos_token_id=tokenizer.get_lang_id("es"))
-        finally:
-            # Capturar la salida de tegrastats
-            #output_tegra, errors_tegra = process_tegra.communicate()
-            process_tegra.terminate()
 
-            # Escribir la salida y errores en el archivo
-            #f.write(f'Tegrastats Output:\n{output_tegra.decode("utf-8")}\n')
-            #f.write(f'Tegrastats Errors:\n{errors_tegra.decode("utf-8")}\n')
+        with torch.no_grad(), profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+            with record_function("model_inference"):
+                generated_tokens = model.generate(input_ids=input_ids, forced_bos_token_id=tokenizer.get_lang_id("es"))
+                
+        #output_tegra, errors_tegra = process_tegra.communicate()
+        process_tegra.terminate()
+
+        # Escribir la salida y errores en el archivo
+        #f.write(f'Tegrastats Output:\n{output_tegra.decode("utf-8")}\n')
+        #f.write(f'Tegrastats Errors:\n{errors_tegra.decode("utf-8")}\n')
 
         # Guardar las métricas del perfilador en el archivo
         model_inference_event = [item for item in prof.key_averages() if item.key == "model_inference"]

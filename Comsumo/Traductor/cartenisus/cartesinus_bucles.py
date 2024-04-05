4bad8c7f41ab9502cdb8594728bb46f44e8d11a6
Vuztulk <luisbalo2001@gmail.com>
2024-04-05 18:02:17 +0200

    a

--- Comsumo/Traductor/cartenisus/cartesinus_bucles.py
+++ Comsumo/Traductor/cartenisus/cartesinus_bucles.py
@@ -8,35 +8,30 @@ import time
 tokenizer = AutoTokenizer.from_pretrained("cartesinus/iva_mt_wslot-m2m100_418M-en-es")
 model = AutoModelForSeq2SeqLM.from_pretrained("cartesinus/iva_mt_wslot-m2m100_418M-en-es")
 
-# Abrimos el archivo de resultados
+# Abrir el archivo de resultados
 with open('resultados.txt', 'w') as f:
-    # Ejecutamos el código 10 veces
+    # Ejecutar el código 10 veces
     for i in range(1):
         start_time = time.time()
 
-        # Leer el texto de entrada desde un archivo .txt
         with open('/home/tfg1/TFG/Problemas/Traductor/input.txt', 'r') as file:
             input_text = file.read().replace('\n', '')
 
         # Codificar entrada
         input_ids = tokenizer.encode(input_text, return_tensors='pt')
 
-        # Realizar la inferencia del modelo con el perfilador
-        with torch.no_grad():
-            with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+        # Ejecutar tegrastats antes de la inferencia
+        process_tegra = subprocess.Popen(['sudo', '/usr/bin/tegrastats'], stdout=subprocess.PIPE)
+
+        try:
+            # Realizar la inferencia del modelo
+            with torch.no_grad(), profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
                 with record_function("model_inference"):
-                    process = subprocess.Popen(['sudo', '/usr/bin/tegrastats'], stdout=subprocess.PIPE)
-                    output_before, _ = process.communicate()
-
                     generated_tokens = model.generate(input_ids=input_ids, forced_bos_token_id=tokenizer.get_lang_id("es"))
+        finally:
 
-                    process = subprocess.Popen(['sudo', '/usr/bin/tegrastats'], stdout=subprocess.PIPE)
-                    output_after, _ = process.communicate()
+            process_tegra.terminate()
 
-                    output_before_str = output_before.decode('utf-8')
-                    output_after_str = output_after.decode('utf-8')
-        print(output_after_str)
-        # Guardamos las métricas del perfilador en el archivo
         model_inference_event = [item for item in prof.key_averages() if item.key == "model_inference"]
         if model_inference_event:
             cpu_time = model_inference_event[0].cpu_time_total
